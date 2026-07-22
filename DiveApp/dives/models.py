86ae3058afba4311091ff19887a179be61ed6f52
sirene0 <sirho.sh02@gmail.com>
2026-07-22 03:30:40 +0100

    add services files

--- DiveApp/dives/models.py
+++ DiveApp/dives/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from users.models import User
-from gases.models import GasMixture
+
 
 
 # Create your models here.
@@ -12,17 +12,17 @@ class DiveStatus (models.TextChoices):
 
 class Dive(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='dives')
-    gas_mix = models.ForeignKey('GasMixture', on_delete=models.CASCADE, related_name='dives')
+    gas_mix = models.ForeignKey('gases.GasMixture', on_delete=models.CASCADE, related_name='dives')
     date = models.DateField()
     depth = models.FloatField()
     duration = models.IntegerField(help_text="Duration in minutes")
     temperature = models.FloatField(help_text="Temperature in Celsius")
     location = models.CharField(max_length=255)
-    ascent_Speed = models.FloatField(help_text="Ascent speed in meters per minute")
+    ascent_speed = models.FloatField(help_text="Ascent speed in meters per minute")
     status = models.CharField(max_length=2, choices=DiveStatus.choices, default=DiveStatus.PLANNED)
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return f"Dive by {self.user.username} on {self.date} at {self.location}"
+        return f"Dive by {self.user.firstname} {self.user.lastname} on {self.date} at {self.location}"
---
+++ DiveApp/services/decompression_Service.py
@@ -0,0 +1,111 @@
+
+from django.core.exceptions import ValidationError
+
+SURFACE_PRESSURE = 1.0  
+N2_SURFACE = 0.79
+WATER_PRESSURE_PER_METER = 0.1
+
+COMPARTMENTS = [
+    {"ID": 1, "half_time": 4.0, "m_value": 3.3},
+    {"ID": 2, "half_time": 8.0, "m_value": 2.8},
+    {"ID": 3, "half_time": 12.5, "m_value": 2.4},
+    {"ID": 4, "half_time": 18.5, "m_value": 2.2},
+    {"ID": 5, "half_time": 27.0, "m_value": 2.0},
+    {"ID": 6, "half_time": 38.3, "m_value": 1.9},
+    {"ID": 7, "half_time": 54.3, "m_value": 1.8},
+    {"ID": 8, "half_time": 77.0, "m_value": 1.7},
+    {"ID": 9, "half_time": 109.0, "m_value": 1.6},
+    {"ID": 10, "half_time": 146.0, "m_value": 1.5},
+    {"ID": 11, "half_time": 187.0, "m_value": 1.5},
+    {"ID": 12, "half_time": 239.0, "m_value": 1.4},
+    {"ID": 13, "half_time": 305.0, "m_value": 1.4},
+    {"ID": 14, "half_time": 390.0, "m_value": 1.3},
+    {"ID": 15, "half_time": 498.0, "m_value": 1.3},
+    {"ID": 16, "half_time": 635.0, "m_value": 1.2},
+]
+
+
+@staticmethod
+def check_safe_ascente_speed(ascend_speed):
+    
+        
+    safe_ascent_speed = 9
+    if ascend_speed > safe_ascent_speed:
+        return {
+            'status': 'danger',
+            'message': f'Ascent speed of {ascend_speed} meters per minute exceeds the safe limit of {safe_ascent_speed} meters per minute.'
+        }
+    else:
+        return {
+            'status': 'safe',
+            'message': f'Ascent speed of {ascend_speed} meters per minute is within the safe limit.'
+        }
+    
+
+
+
+@staticmethod
+def calculate_nitrogen_load(depth, duration,nitrogen_percentage):
+    compartments = calculate_all_compartments(depth, duration, nitrogen_percentage)
+
+    most_loaded = max(compartments, key=lambda c: c['p_final'])
+    all_safe = all(c['is_safe'] for c in compartments)
+
+
+    return {
+        'compartments':   compartments,
+        'most_loaded':    most_loaded,
+        'all_safe':       all_safe,
+        'status': 'safe' if all_safe else 'danger'
+    }
+
+@staticmethod
+def calculate_decompression_Stops(depth ,duration, nitrogen_percentage):
+    result = calculate_nitrogen_load(depth, duration, nitrogen_percentage)
+    if result['status'] == 'safe':
+        return []
+    
+    stops = []
+    current_depth = depth-3
+    while current_depth > 0:
+        compartments= calculate_all_compartments(current_depth, duration, nitrogen_percentage)
+        if all(c['is_safe'] for c in compartments):
+            current_depth -= 3
+            continue
+        most_loaded = max(compartments, key=lambda c: c['p_final'])
+        stop_duration =round(   most_loaded['p_final'] / most_loaded['m_value'] * 10, 1)
+        stops.append({'depth': current_depth, 'duration_min': stop_duration})
+        current_depth -= 3
+
+    return stops
+
+
+def calculate_inspired_n2(depth,nitrogen_percentage):
+    pressure = depth*WATER_PRESSURE_PER_METER + SURFACE_PRESSURE
+    gas_fraction = nitrogen_percentage /100
+    charge_N2 = pressure * gas_fraction 
+    
+    return charge_N2
+
+
+def calculate_compartment_pressure(p_initial, p_inspired, duration, half_time):
+    P_tissu_final = p_initial+ (p_inspired - p_initial) *(1-2**(-duration/half_time))
+    return P_tissu_final
+
+def calculate_all_compartments (depth,duration,nitrogen_percentage):
+    p_inspired = calculate_inspired_n2(depth,nitrogen_percentage)
+    compartment_pressures = []
+    for c in COMPARTMENTS:
+        p_final = calculate_compartment_pressure(
+            p_initial = N2_SURFACE,
+            p_inspired=p_inspired,
+            duration=duration,
+            half_time=c['half_time']
+        )
+        compartment_pressures.append({
+            'compartment_id': c['ID'],
+            'p_final': round(p_final, 4),
+            'm_value': c['m_value'],
+            'is_safe': p_final <= c['m_value']
+        })
+    return compartment_pressures
